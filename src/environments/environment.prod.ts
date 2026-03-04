58435ab24b196d326c36d4a32ce6da037b7760b3
Jonathan Marquina <marquinacancer@gmail.com>
2026-03-04 09:00:29 -0500

    feat: Implementación IA - Cotizaciones EN_EVALUACION v1.0.48

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // appVersion: '1.0.47', // se reemplaza automáticamente
-  appVersion: '1.0.47',
+  // appVersion: '1.0.48', // se reemplaza automáticamente
+  appVersion: '1.0.48',
 
   updateMode: 'AUTO',      // 'AUTO' | 'MANUAL' | 'DISABLED'
   showUpdateModal: true,   // true | false
